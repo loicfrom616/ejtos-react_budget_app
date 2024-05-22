41edd219a99333b7000145c481eb033019cc2cba
loicfrom616 <mawash@mailo.com>
2024-05-21 20:34:18 -0400

    m

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -10,7 +10,7 @@ const Budget = () => {
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" max="20000" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
